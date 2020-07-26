cc035ea25303a0bb106eaa98ed63c9a734be0c45
Navneet Kumar Sharma <navneets816@gmail.com>
2020-07-27 02:04:59 +0530

    Update templates.php

--- admin/themes/templates.php
+++ admin/themes/templates.php
@@ -18,7 +18,7 @@
             <div class="navbar-inner">
                 <div class="container">
                     <a class="btn btn-navbar" data-toggle="collapse" data-target=".navbar-inverse-collapse">
-                        <i class="icon-reorder shaded"></i></a><a class="brand" href="index.html">E-Learning System </a>
+                        <i class="icon-reorder shaded"></i></a><a class="brand" href="index.html">Admin Pannel </a>
                     <div class="nav-collapse collapse navbar-inverse-collapse">
 
                         <ul class="nav pull-right">
