bae67cb68be79a7b347e9a6600be554ec1c922b6
ravikumarsharmaofficial <ravikumarsharmaofficial@gmail.com>
2020-07-29 15:00:24 +0530

    affiliate id generated

--- affiliate_action.php
+++ affiliate_action.php
@@ -20,17 +20,19 @@ if (!$conn) {
 	$phone_number =  $_REQUEST['phone_number'];
 	$email =  $_REQUEST['email'];
 
-/*		function unique_code($limit)
+		function unique_code($limit)
 {
   return substr(base_convert(sha1(uniqid(mt_rand())), 16, 36), 0, $limit);
 }
-*/
-	$refer_id = 77;
+
+	$refer_value = unique_code(8);
+	
+	$refer_id = strval($refer_value);
 
 
 
 $sql = "INSERT INTO affiliate_id ( name, phone_number, email , refer_id )
-VALUES ( '$name', '$phone_number', '$email' , $refer_id )";
+VALUES ( '$name', '$phone_number', '$email' , '$refer_id' )";
 
 if (mysqli_query($conn, $sql)) {
  // $last_id = mysqli_insert_id($conn);
