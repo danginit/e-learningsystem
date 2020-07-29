cad7ceef1d8fa3d6f77dbbdcb1d9744e0377e0c4
danginit <32682428+danginit@users.noreply.github.com>
2020-07-29 18:25:34 +0530

    signup page update

--- login.php
+++ login.php
@@ -1,17 +1,28 @@
 <?php
-
 session_start();
+if(isset($_SESSION['username']))
+{
+	header('location: home.php');
+}
+
+
 ?>
+
 
 
 <!DOCTYPE html>
 <html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="css/style1.css">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Login</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/datepicker3.css" rel="stylesheet">
+	<link href="css/styles4.css" rel="stylesheet">
 </head>
 <body>
+
+
 
 <?php
 
@@ -56,48 +67,29 @@ if(isset($_POST['submit']))
 
 ?>
 
+	<div class="row">
+		<div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
+			<div class="login-panel panel panel-default">
+				<div class="panel-heading">Log in</div>
+				<div class="panel-body">
+					<form action="<?php echo htmlentities($_SERVER['PHP_SELF']);?>" method="POST">
+						<fieldset>
+							<div class="form-group">
+								<input class="form-control" placeholder="E-mail" name="email" type="email" autofocus="">
+							</div>
+							<div class="form-group">
+								<input class="form-control" placeholder="Password" name="password" type="password" value="">
+							</div>
+							<input type="submit" name="submit" class="btn btn-primary" value="Login">
+							<!--<a type="submit" name="submit" class="btn btn-primary">Login</a>--></fieldset>
+					</form>
+				</div>
+			</div>
+		</div><!-- /.col-->
+	</div><!-- /.row -->	
+	
 
-<div class="container" style="margin-left: 250px; margin-right: 250px; margin-top: 100px;">
-  <form action="<?php echo htmlentities($_SERVER['PHP_SELF']);?>" method="POST" style="margin-left: 50px; margin-right: 50px;">
-    <div class="row">
-      <h2 style="text-align:center">Sign In</h2>
-      <div class="vl">
-        <span class="vl-innertext">or</span>
-      </div>
-
-      <div class="col">
-        <a href="#" class="fb btn">
-          <i class="fa fa-facebook fa-fw"></i> Login with Facebook
-         </a>
-        <a href="#" class="google btn"><i class="fa fa-google fa-fw">
-          </i> Login with Google+
-        </a>
-      </div>
-
-      <div class="col">
-        <div class="hide-md-lg">
-          <p>Or sign in manually:</p>
-        </div>
-
-        <input type="email" name="email" placeholder="Email" required>
-        <input type="password" name="password" placeholder="Password" required>
-        <input type="submit" name="submit"  value="Login">
-      </div>
-      
-    </div>
-  </form>
-</div>
-
-<!--- <div class="bottom-container">
-  <div class="row">
-    <div class="col">
-      <a href="#" style="color:white " class="btn">Sign up</a>
-    </div>
-    <div class="col">
-      <a href="#" style="color:white" class="btn">Forgot password?</a>
-    </div>
-  </div>
-</div>  --->
-
+<script src="js/jquery-1.11.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
