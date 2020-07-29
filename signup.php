6a710be3de41083cc979880012811acd9fb450e3
Navneet Kumar Sharma <navneets816@gmail.com>
2020-07-30 00:49:37 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -35,15 +35,15 @@ include 'db_connect.php';
 		$mobile = mysqli_real_escape_string($con, $_POST['mobile']);
 		$password = mysqli_real_escape_string($con, $_POST['password']);
 		$cfmpassword = mysqli_real_escape_string($con, $_POST['cfmpassword']);
-		
+
 		$pass = password_hash($password, PASSWORD_BCRYPT);
 		$cpass = password_hash($cfmpassword, PASSWORD_BCRYPT);
-		
+
 		$emailquery = "select * from registration where email='$email' ";
 		$query = mysqli_query($con,$emailquery);
-		
+
 		$emailcount = mysqli_num_rows($query);
-		
+
 		if($emailcount>0)
 		{
 			echo "email already exist";
@@ -52,11 +52,11 @@ include 'db_connect.php';
 		{
 			if($password === $cfmpassword)
 			{
-				$insertquery = "insert into registration(username,email,mobile,password,cpassword) 
+				$insertquery = "insert into registration(username,email,mobile,password,cpassword)
 				values('$username','$email','$mobile','$pass','$cpass')";
-				
+
 				$iquery = mysqli_query($con, $insertquery);
-				
+
 				if($iquery)
 				{
 					?>
@@ -67,7 +67,7 @@ include 'db_connect.php';
 					header('location: login.php');
 				}else
 				{
-					
+
 					?>
 						<script>
 							alert(" not Inserted");
@@ -106,7 +106,7 @@ include 'db_connect.php';
 							<div class="form-group">
 								<input class="form-control" placeholder="Conform Password" name="cfmpassword" type="password" value="">
 							</div>
-							
+
 							<input type="submit" name="submit" class="btn btn-primary" value="Sign Up">
 							<div>
 								<p>Have an account? <a href="login.php">Login</a><p>
@@ -116,8 +116,8 @@ include 'db_connect.php';
 				</div>
 			</div>
 		</div><!-- /.col-->
-	</div><!-- /.row -->	
-	
+	</div><!-- /.row -->
+
 
 <script src="js/jquery-1.11.1.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
