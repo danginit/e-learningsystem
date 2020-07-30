13715f427f4e3cb23133357ec0664613cb440417
ravikumarsharmaofficial <ravikumarsharmaofficial@gmail.com>
2020-07-31 03:45:31 +0530

    Affiliate page added to main index page

---
+++ Affiliate/Affiliate_page.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sign Up Form by Colorlib</title>
+
+    <!-- Font Icon -->
+    <link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
+
+    <!-- Main css -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <div class="main">
+
+        
+        
+        <section class="sign-in">
+            <div class="container">
+                <div class="signin-content">
+                    <div class="signin-image">
+                        <figure><img src="images/signin-image.jpg" alt="sing up image"></figure>
+                        
+                    </div>
+
+                    <div class="signin-form">
+                        <h2 class="form-title">Affiliate</h2>
+                        <form method="POST" class="register-form" id="login-form" action="affiliate_action.php">
+                            <div class="form-group">
+                                <label for="your_name"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                                <input type="text" name="name" id="name" placeholder="Enter Your Name"/>
+                            </div>
+                            <div class="form-group">
+                                <label for="your_phone"><i class="zmdi zmdi-phone"></i></label>
+                                <input type="Phone" name="phone_number" id="phone_number" placeholder="Enter Your Phone Number"/>
+                            </div>
+                            <div class="form-group">
+                                <label for="your_email"><i class="zmdi zmdi-email"></i></label>
+                                <input type="text" name="email" id="email" placeholder="Enter Your Email id"/>
+                            </div>
+                            <div class="form-group form-button">
+                                <input type="submit" name="btnRegister" id="btnRegister" class="form-submit" value="Register"/>
+                            </div>
+                        </form>
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </div>
+
+    <!-- JS -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
