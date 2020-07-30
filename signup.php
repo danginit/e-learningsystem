837ff57d67399b498fb34d31b19ac98c00e34915
Navneet Kumar Sharma <navneets816@gmail.com>
2020-07-31 02:31:32 +0530

    test

--- signup.php
+++ signup.php
@@ -87,7 +87,7 @@ include 'db_connect.php';
 	<div class="row">
 		<div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
 			<div class="login-panel panel panel-default">
-				<div class="panel-heading">SignUp</div>
+				<div class="panel-heading">Log in</div>
 				<div class="panel-body">
 					<form action="<?php echo htmlentities($_SERVER['PHP_SELF']);?>" method="POST">
 						<fieldset>
